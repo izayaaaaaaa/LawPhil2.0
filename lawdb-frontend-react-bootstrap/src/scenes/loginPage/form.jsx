9c733bf269649c1f9a2bff41769c92578bd67d09
niltiac2Ds <caitlinDSda2@gmail.com>
2023-07-11 22:37:52 +0800

    merged changes from loginPage commit from visuals branch

--- lawdb-frontend-react-bootstrap/src/scenes/loginPage/form.jsx
+++ lawdb-frontend-react-bootstrap/src/scenes/loginPage/form.jsx
@@ -1,39 +1,61 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../styles/general.css';
+import '../../styles/login.css';
 
 const Form = () => {
   return (
-    <div class="row">
-      <div class="col-md-auto">
-        <h3>Welcome back to</h3>
-        <h1>LawPhil!</h1>
+      <div className="container px-5">
+        <div className="row d-flex justify-content-center align-items-center">
+          <div className="col col-xl-10">
+            <div className="card">
+              <div className="row g-0">
+              {/* Login Container - Welcome Message */}
+                <div className="col-md-6 col-lg-6 d-flex flex-column justify-content-center align-items-center text-center" id="welcome-side">
+                    <h3>Welcome back to</h3>
+                    <h2>LawPhil Project!</h2>
+                </div>
+
+              {/* Login Container - Login Form */}
+                <div className="col-md-6 col-lg-6 d-flex align-items-center" id="login-side">
+                  <div className="card-body p-4 p-lg-5 text-black">
+                    <form>
+                      <div className="form-outline mb-4">
+                        <label className="form-label" for="form-username">Username</label>
+                        <input 
+                          type="email" 
+                          id="form-username" 
+                          className="form-control form-control-md" 
+                        />
+                      </div>
+                      
+                      <div className="form-outline mb-4">
+                        <label className="form-label" for="form-password">Password</label>
+                        <input 
+                          type="password" 
+                          id="form-password" 
+                          className="form-control form-control-md"/>
+                      </div>
+                  
+                      <div className="md-auto mx-auto">
+                        <div className="text-center mt-4 mx-auto">
+                          <a href="#!" className="link-light">Forgot Password?</a>
+                        </div>
+                    
+                        <div className="text-center mt-4 mx-auto">
+                          <button type="button" className="btn btn-light btn-md">
+                            <text id="btn-text">Login</text>
+                          </button>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="col-md-auto">
-        <form>
-          <div class="form-outline mb-4">
-            <label class="form-label" for="form3Example3">Username</label>
-            <input 
-              type="email" 
-              id="form3Example3" 
-              class="form-control form-control-lg" 
-            />
-          </div>
-          
-          <div class="form-outline mb-3">
-            <label class="form-label" for="form3Example4">Password</label>
-            <input type="password" id="form3Example4" class="form-control form-control-lg"/>
-          </div>
-      
-          <div class="d-flex justify-content-between align-items-center">
-            <a href="#!" class="text-body">Forgot password?</a>
-          </div>
-      
-          <div class="text-center text-lg-start mt-4 pt-2">
-            <button type="button" class="btn btn-primary btn-lg">Login</button>
-          </div>
-        </form>
-      </div>
-    </div>
   );
 };
 
--- lawdb-frontend-react-bootstrap/src/scenes/registerPage/Form.jsx
+++ lawdb-frontend-react-bootstrap/src/scenes/registerPage/Form.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
+// import * as yup from 'yup';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// const schema = yup.object().shape({
+//   username: yup.string().required(),
+//   email: yup.string().email().required(),
+//   password: yup.string().required(),
+//   confirmPassword: yup.string().required(),
+// });
+
+// const initialValues = {
+//   username: '',
+//   email: '',
+//   password: '',
+//   confirmPassword: '',
+// };
+
 const Form = () => {
+  const handleSubmit = async(values) => {
+    const formData = new FormData();
+    for (let value in values) {
+      formData.append(value, values[value]);
+    }
+
+    const savedUserResponse = await fetch(
+      "http://localhost:3001/auth/register",
+      {
+          method: "POST",
+          body: formData,
+      }
+    );
+    const savedUser = await savedUserResponse.json();
+    // onSubmitProps.resetForm(); RESET THE FORM
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div class="form-outline mb-4">
         <label class="form-label" for="form3Example3">Username</label>
         <input 
@@ -13,10 +45,10 @@ const Form = () => {
         />
       </div>
       <div class="form-outline mb-4">
-        <label class="form-label" for="form3Example3">Username</label>
+        <label class="form-label" for="form4Example4">Username</label>
         <input 
-          type="email" 
-          id="form3Example3" 
+          type="text" 
+          id="form4Example4" 
           class="form-control form-control-lg" 
         />
       </div>
@@ -31,7 +63,7 @@ const Form = () => {
       </div>
   
       <div class="text-center text-lg-start mt-4 pt-2">
-        <button type="button" class="btn btn-primary btn-lg">Register</button>
+        <button type="submit" class="btn btn-primary btn-lg">Register</button>
       </div>
     </form>
   );
