2a1bb180fa1cc3ff258404d16c628a85bf2f263f
izayaaaaaa <izaya.orihara0011@gmail.com>
2023-09-13 03:57:04 +0800

    search is working the way it should be (small spacing bugs on the search results page btw)

--- lawdb-frontend-react-bootstrap/src/scenes/searchPage/searchPage.jsx
+++ lawdb-frontend-react-bootstrap/src/scenes/searchPage/searchPage.jsx
@@ -1,22 +1,21 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 import Form from './Form';
-import SearchResults from '../searchResultsPage/SearchResults';
 import '../../styles/general.css';
 import '../../styles/search.css';
+import { useNavigate } from 'react-router-dom';
 
 // searchPage.jsx
 
 const SearchPage = ({ hostUrl }) => {
   const [searchQuery, setSearchQuery] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
+  const navigate = useNavigate();
 
   const fetchSearchResults = useCallback(async () => {
     try {
       console.log('Fetching with query:', searchQuery);
       const response = await axios.get(`${hostUrl}/LawPhil2.0_Server/search.php?q=${searchQuery}`);
       console.log('Response:', response);
-      setSearchResults(response.data);
     } catch (error) {
       console.error('Error fetching search results:', error);
       if (error.response) {
@@ -32,13 +31,13 @@ const SearchPage = ({ hostUrl }) => {
 
   const handleSearch = (query) => {
     setSearchQuery(query);
+    navigate(`/search-results/${query}`);
   };
 
   return (
     <div>
       <h1>Search Page</h1>
       <Form onSearch={handleSearch} />
-      <SearchResults results={searchResults} />
     </div>
   );
 };
---
+++ lawdb-frontend-react-bootstrap/src/scenes/searchResultsPage/index.jsx
@@ -0,0 +1,37 @@
+// import modules
+import { useParams } from 'react-router-dom';
+import Axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import SearchResults from './SearchResults';
+
+const SearchResultsPage = ({ hostUrl }) => {
+  const { query } = useParams();
+  const [searchResults, setSearchResults] = useState([]); 
+
+  useEffect(() => {
+    // Fetch search results based on the query
+    Axios.get(`${hostUrl}/LawPhil2.0_Server/search.php?q=${query}`)
+      .then((response) => {
+        // Handle the successful response
+        console.log('Search query:', query);
+        console.log('Search results:', response.data);
+        setSearchResults(response.data);
+      })
+      .catch((error) => {
+        // Handle errors
+        console.error('Error fetching search results:', error);
+        if (error.response) {
+          console.error('Response data:', error.response.data);
+          console.error('Response status:', error.response.status);
+        }
+      });
+  }, [query]);
+
+  return (
+    <div>
+      <SearchResults results={searchResults} />
+    </div>
+  );
+}
+
+export default SearchResultsPage;
