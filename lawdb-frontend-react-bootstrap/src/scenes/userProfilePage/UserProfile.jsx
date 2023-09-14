3b4b41e05864ac715522d1ba9692755440ed8eb9
izayaaaaaa <izaya.orihara0011@gmail.com>
2023-09-14 23:27:30 +0800

    modified UserProfile to avoid it from bugging out (not fully functional yet)

--- lawdb-frontend-react-bootstrap/src/scenes/userProfilePage/UserProfile.jsx
+++ lawdb-frontend-react-bootstrap/src/scenes/userProfilePage/UserProfile.jsx
@@ -1,110 +1,99 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import '../../styles/general.css';
-import '../../styles/userprofile.css';
 
 const UserProfile = ({ hostUrl }) => {
-  // State to hold user profile details fetched from the database
-  const [userProfile, setUserProfile] = useState(null);
-  // State to track if the component is in edit mode
-  const [editMode, setEditMode] = useState(false);
-  // State to track if changes have been saved
-  const [changesSaved, setChangesSaved] = useState(false);
+  const [userData, setUserData] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedData, setEditedData] = useState({
+    username: '',
+    email: '',
+  });
 
-  // Function to fetch user profile details from the database
+  const userId = localStorage.getItem('id');
+
   useEffect(() => {
+    // Fetch user data and populate the state
     fetch(`${hostUrl}/LawPhil2.0_Server/getUserProfile.php?userId=${userId}`)
       .then(async (response) => {
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
-        const data = await response.json(); // Parse the JSON response
-        console.log('Response:', data); // Log the data, not the response.json() function
-        return data; // Return the parsed data
-      })
-      .then((data) => {
-        // Update the state with the user data
+        const data = await response.json();
         setUserData(data);
+        setEditedData(data); // Initialize editedData with user data
       })
       .catch((error) => {
         console.error('Error fetching user data:', error);
       });
   }, [userId, hostUrl]);
 
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleSaveClick = () => {
+    // Send an update request to the backend
+    fetch(`${hostUrl}/LawPhil2.0_Server/updateUserProfile.php?userId=${userId}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(editedData),
+    })
+      .then(async (response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        console.log('Update response:', data);
+        setIsEditing(false); // Disable editing mode after successful update
+      })
+      .catch((error) => {
+        console.error('Error updating user profile:', error);
+      });
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setEditedData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
   if (!userData) {
-    // log userdata
-    console.log('userData:', userData);
     return <div>Loading...</div>;
   }
 
   return (
-    <div className="container position-relative">
-      {/* Show alert when changes are saved */}
-      {changesSaved && (
-        <div className="alert alert-success position-absolute top-0 end-0 mt-3 me-3" role="alert">
-          Changes have been saved!
+    <div>
+      <h1>User Profile</h1>
+      {isEditing ? (
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            value={editedData.username}
+            onChange={handleInputChange}
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={editedData.email}
+            onChange={handleInputChange}
+          />
+          <button onClick={handleSaveClick}>Save</button>
+        </div>
+      ) : (
+        <div>
+          <p>Name: {userData.username}</p>
+          <p>Email: {userData.email}</p>
+          <button onClick={handleEditClick}>Edit Profile</button>
         </div>
       )}
-      <h2 className="text-start mb-5">Hi, {userProfile.username}!</h2>
-      <div className="row">
-        {/* Profile Picture (Left Column) */}
-        <div className="col-md-6 d-flex align-items-center justify-content-center mt-3">
-          {/* Check if userProfile has a profile picture */}
-          {userProfile.profilePicture ? (
-            <img src={userProfile.profilePicture} alt="Profile" className="img-fluid pfp" />
-          ) : (
-            <img src="/logo.png" alt="Default Profile" className="pfp img-fluid" />
-          )}
-        </div>
-
-        {/* User Profile Details (Right Column) */}
-        <div className="col-md-6">
-          <form>
-            <div className="mb-3">
-              <label htmlFor="username" className="form-label">
-                Username
-              </label>
-              <input
-                type="text"
-                className="form-control prof-form-control"
-                id="username"
-                value={userProfile.username}
-                readOnly={!editMode}
-                onChange={(e) => setUserProfile({ ...userProfile, username: e.target.value })}
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="email" className="form-label">
-                Email
-              </label>
-              <input
-                type="email"
-                className="form-control prof-form-control"
-                id="email"
-                value={userProfile.email}
-                readOnly={!editMode}
-                onChange={(e) => setUserProfile({ ...userProfile, email: e.target.value })}
-              />
-            </div>
-            {/* Password change - should be the same as Forgot Password */}
-            <a href="#!" className="link-dark">Request Password Change</a>
-            <br />
-          </form>
-        </div>
-      </div>
-      {/* Buttons - Aligned to the bottom right corner */}
-      <div className="d-flex justify-content-end end-0 my-3">
-        {/* Toggle between View/Edit mode */}
-        <button type="button" className="btn edit-btn" onClick={() => setEditMode(!editMode)}>
-          {editMode ? 'Cancel' : 'Edit'}
-        </button>
-        {/* Show "Save Changes" button only in Edit mode */}
-        {editMode && (
-          <button type="submit" className="btn save-btn ms-2" onClick={handleSaveChanges}>
-            Save Changes
-          </button>
-        )}
-      </div>
     </div>
   );
 };
