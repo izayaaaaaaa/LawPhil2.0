6119589b4ac667f65911f0675ab136feb8f82f85
izayaaaaaa <izaya.orihara0011@gmail.com>
2023-09-18 06:09:35 +0000

    added "dev containers"

--- lawdb-frontend-react-bootstrap/src/App.js
+++ lawdb-frontend-react-bootstrap/src/App.js
@@ -7,7 +7,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 import LoginPage from './scenes/loginPage';
-import NavbarPage from './scenes/navbarPage';
+import NavbarPage from './scenes/navbar';
 import RegisterPage from './scenes/registerPage';
 import SearchPage from './scenes/searchPage/searchPage';
 import SearchResultsPage from './scenes/searchResultsPage';
@@ -17,7 +17,7 @@ import AdminDashboard from './scenes/adminDashboardPage';
 import './styles/components.css';
 import './styles/general.css';
 
-const hostUrl = "http://192.168.100.19"; 
+const hostUrl = "http://192.168.100.19";
 
 const BackgroundWrapper = ({ children }) => {
   const location = useLocation();
@@ -36,20 +36,20 @@ const BackgroundWrapper = ({ children }) => {
   // Conditionally apply padding for search results page
   const getContentStyles = () => {
     let paddingTop = '12vh'; // Default top padding
-  
+
     if (location.pathname === '/law-content/') {
       paddingTop = '30vh'; // Override top padding for search results page
     } else if (location.pathname === '/user-profile' || location.pathname === '/login') {
       paddingTop = '25vh'; // Override top padding for search results page
-    }else if (location.pathname === '/admin-dashboard') {
+    } else if (location.pathname === '/admin-dashboard') {
       paddingTop = '15vh'; // Override top padding for admin-dashboard page
     } else if (location.pathname === '/') {
       paddingTop = '0'; // Override top padding for search page
     }
-  
+
     return { paddingTop, paddingBottom: '0' }; // Set bottom padding to 0 for all pages
   };
-  
+
 
   return (
     <div className={`App ${getBackgroundClass()}`}>
@@ -75,9 +75,9 @@ function App() {
 
             <Route path="/login" element={<LoginPage hostUrl={hostUrl} />} />
             <Route path="/register" element={<RegisterPage hostUrl={hostUrl} />} />
-            <Route path="/user-profile/:id" element={<UserProfile hostUrl={hostUrl}/>} />
-            <Route path="/admin-dashboard" element={<AdminDashboard hostUrl={hostUrl}/>} />
-      
+            <Route path="/user-profile/:id" element={<UserProfile hostUrl={hostUrl} />} />
+            <Route path="/admin-dashboard" element={<AdminDashboard hostUrl={hostUrl} />} />
+
             {/* about us route */}
             {/* disclaimer route ? */}
           </Routes>
