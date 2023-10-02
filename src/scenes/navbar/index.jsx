4f23c59b7afe3ddedba7c6de199b2d424155f6a0
niltiac2Ds <caitlinDSda2@gmail.com>
2023-10-03 01:10:12 +0800

    fixed navbar, search and results ui

--- src/scenes/navbar/index.jsx
+++ src/scenes/navbar/index.jsx
@@ -1,30 +1,44 @@
 // navbar
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretDown, faSearch } from '@fortawesome/free-solid-svg-icons'; // Assuming you have the search icon imported
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import '../../styles/general.css';
 import '../../styles/navbar.css';
 
 const Navbar = () => {
+  const location = useLocation();
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        {/* Navbar dropdown */}
-        <div id="navbar-dropdown">
-          <img src="/logo.png" className="logo-sm mx-3" alt="LawPhil Logo" />
-
-          {/* Dropdown content */}
-          <div className="dropdown-content">
-            <Link to="/link1">Log In</Link>
-            <Link to="/admin-dashboard">Admin Dashboard</Link>
-            <Link to="/link3">Example</Link>
+        
+        <div className="btn-group">
+          <div className="dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+            <img src="/logo.png" className="logo-sm mx-2"/>
           </div>
+          <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <li><Link to="/link1">Log In</Link></li>
+            <li><Link to="/admin-dashboard">Admin Dashboard</Link></li>
+            <li><Link to="/link3">Example</Link></li>
+          </ul>
         </div>
-<Link className="navbar-brand" to="/">ARELLANO LAW FOUNDATION</Link>
-        <Link className="nav-link active" aria-current="page" to="/">
-          LAWS
-        </Link>
+
+        <Link className="navbar-brand" to="/">ARELLANO LAW FOUNDATION</Link>
+
+        {location.pathname.startsWith("/search-results") ? (
+          <Link className="nav-link active" aria-current="page" to="/">
+            <FontAwesomeIcon icon={faSearch} />
+          </Link>
+        ) : (
+          <Link className="nav-link active" aria-current="page" to="/"> {/* change to law list when available */}
+            LAWS
+          </Link>
+        )}
+
+        
       </div>
     </nav>
   );
