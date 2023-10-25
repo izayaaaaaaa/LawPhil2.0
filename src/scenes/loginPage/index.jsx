9e447b4f9a22c396e52a31cd4143fd1860b2b560
izayaaaaaaa <izaya.orihara0011@gmail.com>
2023-10-26 07:56:01 +0800

    feat: login, register, and user profile existing code

---
+++ src/scenes/loginPage/index.jsx
@@ -0,0 +1,72 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import '../../styles/general.css';
+import '../../styles/login.css';
+import Form from "./Form";
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+
+const LoginPage = ({ hostUrl }) => {
+    const navigate = useNavigate();
+    
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
+    
+    // SETS THE REQUEST BODY TO BE SENT TO THE SERVER
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.id]: e.target.value,
+        });
+    };
+    
+    // LOGIN API
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+        }
+        
+        const url = `${hostUrl}/LawPhil2.0_Server/userCRUD/loginUser.php`;
+        
+        try {
+            const response = await fetch(url, requestOptions);
+            
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            
+            const responseData = await response.json();
+            
+            if (responseData.success) {
+                const redirectUrl = responseData.role === 'admin' ? '/admin-dashboard' : '/';
+                navigate(redirectUrl);
+            } else {
+                console.log("Login failed:", responseData.message);
+            }
+        } catch (error) {
+            console.error("Login failed:", error.message);
+        }
+    };
+    
+    return (
+        <div>
+            <div className="container-user d-flex align-items-center min-vh-100">
+                <Form
+                    className="text-center"
+                    formData={formData}
+                    handleChange={handleChange}
+                    handleSubmit={handleSubmit}
+                />
+            </div>
+        </div>
+        );
+    };
+    
+    export default LoginPage;
