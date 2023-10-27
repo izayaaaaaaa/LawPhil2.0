2ed743823cdf923a553a7b16c08782c27fb10632
izayaaaaaaa <izaya.orihara0011@gmail.com>
2023-10-28 05:26:28 +0800

    fix: infinity rendering bug on login

--- src/scenes/loginPage/index.jsx
+++ src/scenes/loginPage/index.jsx
@@ -1,10 +1,10 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { UserContext } from '../../userContext';
+import { useNavigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import '../../styles/general.css';
 import '../../styles/login.css';
 import Form from "./Form";
-import React, { useState } from 'react';
-import { useNavigate } from "react-router-dom";
-
 // TODO:
 //  1. feature to navigate to admin dashboard if account is an admin account
 
@@ -12,22 +12,45 @@ import { useNavigate } from "react-router-dom";
 //  1. login form position is off
 
 const LoginPage = ({ hostUrl }) => {
+    const { user, dispatch } = useContext(UserContext);   
     const navigate = useNavigate();
     
     const [formData, setFormData] = useState({
         email: '',
         password: '',
-    });
-    
-    // SETS THE REQUEST BODY TO BE SENT TO THE SERVER
+    });    
+
+    const [shouldNavigate, setShouldNavigate] = useState(false);
+
     const handleChange = (e) => {
         setFormData({
             ...formData,
             [e.target.id]: e.target.value,
         });
     };
+
+    // This effect redirects the user after login, based on their role
+    useEffect(() => {
+        if (user && shouldNavigate) {
+            console.log('Preparing to navigate...');  // Log for debugging
+            console.log('Current user object:', user);
+            // Reset the navigation flag
+            setShouldNavigate(false);
+
+            // Determine the destination based on the user's role
+            const destination = user.is_admin === 1 ? '/admin-dashboard' : '/search';
+
+            // Navigate to the new destination
+            try {
+                console.log('Navigated to:');
+                navigate(destination);
+                console.log(destination);
+            } catch (error) {
+                console.error('Navigation error:', error);
+            }
+        }
+    }, [user, navigate, shouldNavigate]);
     
-    // LOGIN API
     const handleSubmit = async (e) => {
         e.preventDefault();
         
@@ -37,25 +60,33 @@ const LoginPage = ({ hostUrl }) => {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify(formData),
-        }
+        };
         
         const url = `${hostUrl}/LawPhil2.0_Server/userCRUD/loginUser.php`;
         
         try {
             const response = await fetch(url, requestOptions);
-            
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            
             const responseData = await response.json();
+
+            console.log('Response Data: ', responseData);
             
-            if (responseData.success) {
-                if (responseData.is_admin === 1) { // or use "role" if your response uses role instead of is_admin
-                    navigate('/admin-dashboard');
-                } else {
-                    navigate('/search');
-                }
+            if (responseData.success) {  // Only set user if login is successful and user is not set
+                const newUser = {
+                    id: responseData.id,
+                    email: responseData.email,
+                    is_admin: responseData.is_admin,
+                };
+    
+                console.log('Dispatching user:', newUser);  // Log the user being dispatched
+    
+                // Dispatch the SET_USER action to update the global user state upon successful login
+                dispatch({
+                    type: 'SET_USER',
+                    payload: newUser,
+                });
+    
+                // Set the navigation flag
+                setShouldNavigate(true);
             } else {
                 console.log("Login failed:", responseData.message);
             }
@@ -75,7 +106,7 @@ const LoginPage = ({ hostUrl }) => {
                 />
             </div>
         </div>
-        );
+    );
 };
     
 export default LoginPage;
